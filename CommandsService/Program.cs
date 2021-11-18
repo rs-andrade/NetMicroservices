47dcad4cde999b9401ac903f15b46f2ad5a390e4
Rodrigo Andrade <rodrigo.andrade@skyneti.com.br>
2021-11-17 21:05:29 -0300

    Command Controller

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,8 +1,14 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMen"));
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
